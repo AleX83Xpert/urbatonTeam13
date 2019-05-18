d0b001c98d7c40c937cd0b52268baef9c686f5a4
Daniil Galiev <dgaliev@rc-online.org>
2019-05-18 19:05:10 +0500

    api interface

--- gc_core/gc_core/blueprints/claim.py
+++ gc_core/gc_core/blueprints/claim.py
@@ -4,10 +4,13 @@ from webargs.flaskparser import use_args
 
 bp_claim = Blueprint("claim", __name__)
 
-hello_args = {"citizen_id": fields.Int(required=True)}
+hello_args = {
+    "garbage_type": fields.Str(required=True),
+    "parameters": fields.Str(required=True),
+}
 
 
-@bp_claim.route('/<collector_id>', methods=["POST"])
+@bp_claim.route('/<collector_id>/<citizen_id>', methods=["POST"])
 @use_args(hello_args)
-def get(collector_id, args):
-    return jsonify({"ok"})
+def get(collector_id, citizen_id, args):
+    return jsonify([collector_id, citizen_id, args])
