fbb8c0a324846c60f1c1536c32de57585a0f5343
Daria Svalova <svalova_d@skbkontur.ru>
2019-05-19 00:02:24 +0500

    add citizen search

--- gc_core/gc_core/blueprints/citizen.py
+++ gc_core/gc_core/blueprints/citizen.py
@@ -2,14 +2,32 @@ from flask import Blueprint, jsonify
 from webargs import fields
 from webargs.flaskparser import use_args
 
+from . import goodObject
+import pymysql.cursors
+
 bp_citizen = Blueprint("citizens", __name__)
 
 search_args = {
     "login": fields.Str(required=False)
 }
 
-
-@bp_citizen.route('/search', methods=["GET"])
+host="localhost"
+user="user"
+password="passwd"
+db="db"
+charset="utf8mb4"
+cursorclass=pymysql.cursors.DictCursor
+ 
+@bp_citizen.route("/search", methods=["GET"])
 @use_args(search_args)
 def search(args):
-    return jsonify(args)
+    print("searching citizens by login=" + args["login"])
+    users = search_by(args["login"])
+    return jsonify(users)
+
+def search_by(login):
+    request = "SELECT `id`, `login` FROM `users` WHERE `type`='citizen' AND `login` LIKE %s"
+    with goodObject.connection.cursor() as cursor:
+        cursor.execute(request, ('%' + login + '%'))
+        result = cursor.fetchall()
+        return result
