e10b6614d8a096fe30632697935325f65505608d
Daniil Galiev <dgaliev@rc-online.org>
2019-05-19 02:16:20 +0500

    session

--- gc_core/gc_core/app_factory.py
+++ gc_core/gc_core/app_factory.py
@@ -2,12 +2,14 @@ import logging
 from flask import Flask
 
 from gc_core.mount import mount
+from gc_core.utils import sessionify
 
 
 def factory():
     setup_logging()
     app = Flask(__name__)
     mount(app)
+    sessionify(app)
     return app
 
 
--- gc_core/gc_core/blueprints/test.py
+++ gc_core/gc_core/blueprints/test.py
@@ -1,8 +1,16 @@
 from flask import Blueprint
 
+from gc_core.utils import for_collector
+
 bp_test = Blueprint("test", __name__)
 
 
-@bp_test.route('/ping', methods=["GET"])
+@bp_test.route("/ping", methods=["GET"])
 def ping():
     return "pong"
+
+
+@bp_test.route("/with_role")
+@for_collector
+def with_role():
+    return "OK"
