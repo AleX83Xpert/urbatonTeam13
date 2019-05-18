f35d31f1c9dff3651bcfb39eca2a978f4d3d780b
Daria Svalova <svalova_d@skbkontur.ru>
2019-05-19 01:42:16 +0500

    add collectors api

--- gc_core/gc_core/blueprints/collector.py
+++ gc_core/gc_core/blueprints/collector.py
@@ -13,4 +13,17 @@ search_args = {
 @bp_collector.route('/search', methods=["GET"])
 @use_args(search_args)
 def search(args):
-    return jsonify(args)
+    print("searching collectors by garbageType=" + args["garbageType"])
+    collectors = search_by(args["garbageType"])
+    return jsonify(collectors)
+
+def search_by(garbageType):
+    request = """SELECT delivery_points.*
+                 FROM `users` JOIN delivery_points ON `users`.id = delivery_points.user
+                    JOIN delivery_point_garbage_types ON delivery_points.id = delivery_point_garbage_types.`point`
+                    JOIN garbage_types ON garbage_types.code = delivery_point_garbage_types.`type`
+                 WHERE `users`.type='collector' AND garbage_types.code=%s"""
+    with goodObject.connection.cursor() as cursor:
+        cursor.execute(request, (garbageType))
+        result = cursor.fetchall()
+        return result
