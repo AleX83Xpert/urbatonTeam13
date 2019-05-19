346189f02717198f076457a24c135b6e331a432c
Your Name <you@example.com>
2019-05-19 03:56:18 +0000

    static

--- gc_core/entry.py
+++ gc_core/entry.py
@@ -1,6 +1,13 @@
+from flask import render_template
+
 from gc_core.app_factory import factory
 
 
 if __name__ == "__main__":
     app = factory()
+
+    @app.route("/")
+    def front():
+        return render_template('index.html')
+
     app.run("0.0.0.0", 8082, debug=True)
